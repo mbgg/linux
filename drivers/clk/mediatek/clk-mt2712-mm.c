d29ee482a265b0720c4434e4b7347792b305fae7
Matthias Brugger <mbrugger@suse.com>
2018-11-16 00:36:04 +0100

    clk: mediatek: mt2712e: Probe with new compatible
    The clock node is now a child of the mmsys node.
    Update the driver to support this and thenew compatible
    in the driver.
    
    Signed-off-by: Matthias Brugger <mbrugger@suse.com>

--- drivers/clk/mediatek/clk-mt2712-mm.c
+++ drivers/clk/mediatek/clk-mt2712-mm.c
@@ -130,14 +130,15 @@ static int clk_mt2712_mm_probe(struct platform_device *pdev)
 {
 	struct clk_onecell_data *clk_data;
 	int r;
-	struct device_node *node = pdev->dev.of_node;
+	struct device	parent = pdev->dev.parent;
 
 	clk_data = mtk_alloc_clk_data(CLK_MM_NR_CLK);
 
-	mtk_clk_register_gates(node, mm_clks, ARRAY_SIZE(mm_clks),
+	mtk_clk_register_gates(parent->of_node, mm_clks, ARRAY_SIZE(mm_clks),
 			clk_data);
 
-	r = of_clk_add_provider(node, of_clk_src_onecell_get, clk_data);
+	r = of_clk_add_provider(parent->of_node, of_clk_src_onecell_get,
+			clk_data);
 
 	if (r != 0)
 		pr_err("%s(): could not register clock provider: %d\n",
@@ -147,7 +148,7 @@ static int clk_mt2712_mm_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id of_match_clk_mt2712_mm[] = {
-	{ .compatible = "mediatek,mt2712-mmsys", },
+	{ .compatible = "mediatek,mt2712-mmsys-clk", },
 	{}
 };
 
